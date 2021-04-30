2636da4259a2042525ed7b6072f250d94afe8a51
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-04-30 18:26:14 +0200

    Pokedex finished on class - WK2

--- src/Logo.js
+++ src/Logo.js
@@ -1,23 +1,12 @@
 const Logo = (props) => {
 
-//   var appName = "Rafael Flores's Pokedex"
-
   return (
       <header>
       <h1>Welcome to {props.appName}</h1>
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"/>
+      <img onClick= {props.onClick} 
+      src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"/>
       </header>  
   );
 }
 
 export default Logo;
-
-//Importing components//
-
-function ImportLogo() {
-  return (
-    <div>
-      <Logo />
-    </div>
-  );
-}
